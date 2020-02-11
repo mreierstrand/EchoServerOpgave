a78ffd333164a5b65aef0bd203e76b522047a957
Oliver Lasse Eierstrand <oliv1115@edu.easj.dk>
2020-02-11 14:00:27 +0100

    Add project files.

---
+++ EchoClient/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace EchoClient
+{
+    class EchoClient
+    {
+        static void Main(string[] args)
+        {
+            StartClient();
+        }
+
+        public static void StartClient()
+        {
+            TcpClient socket = new TcpClient("192.168.24.205", 6546);
+            Stream ns = socket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+            string message = Console.ReadLine();
+            sw.WriteLine(message);
+            string line = sr.ReadLine();
+
+            Console.WriteLine(line);
+        }
+
+
+    }
+}
---
+++ EchoServer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EchoServer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ip = IPAddress.Parse("192.168.24.205");
+            TcpListener serverSocket = new TcpListener(ip, 6789);
+
+            serverSocket.Start();
+            Console.WriteLine("Server Started");
+
+            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
+            Console.WriteLine("Server Activated");
+
+            DoClient(connectionSocket);
+
+            serverSocket.Stop();
+
+        }
+
+
+        public static void DoClient(TcpClient socket)
+        {
+            Stream ns = socket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+            string message = sr.ReadLine();
+            string answer = "";
+
+            while (!string.IsNullOrEmpty(message))
+            {
+                Console.WriteLine("Client:" + message);
+                answer = message.ToUpper();
+                sw.WriteLine(answer);
+                message = sr.ReadLine();
+            }
+
+            ns.Close();
+            socket.Close();
+
+        }
+
+
+
+    }
+}
